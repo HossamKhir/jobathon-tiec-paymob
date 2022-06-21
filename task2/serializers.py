9db20bfb5cb9c7ca56a60f7c656816c441d7383e
Hossam Khair <hossam.khir1@outlook.com>
2022-06-21 11:18:19 +0200

    feat: setting up task 2

---
+++ task2/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+
+    # add code here
+
+    pass
---
+++ task2/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from rest_framework import permissions
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows persons to be viewed.
+    """
+    # add code here
+    pass
